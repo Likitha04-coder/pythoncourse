5a585e2ded6ec79a4550d28bb6b5ddeb200ac4dd
Likitha04-coder <likithap2404@gmail.com>
2026-02-25 12:32:52 +0000

    Day 1: Learned print and variables

---
+++ first.py
@@ -0,0 +1,10 @@
+print("hello worl")
+print("my name is likitha")
+print(4-6)
+print(3*7)
+
+name="likitha"
+age=25
+print("my name is",name);
+print("i'm",age,"years old");
+print("learning python is fun");
